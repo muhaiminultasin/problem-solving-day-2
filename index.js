659d220c7630ecc62b1a68ca465ea12e09d2d986
muhaiminultasin <muhaiminulislamtasin@gmail.com>
2024-07-28 00:22:26 +0600

    time up for poor network

--- index.js
+++ index.js
@@ -61,3 +61,48 @@ class Circle {
 let myRec2 = new Circle(10)
 let myArea2 = myRec.calculateArea2();
 console.log(myArea2)
+
+// 6. 
+
+const one = 'name';
+const two = 'age';
+const three = 'job';
+
+const man = {
+  [one]: 'John Doe',
+  [two]: 30,
+  [three]: 'Developer'
+};
+
+// console.log(man);
+
+// 7.
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        return `${this.name} is barking`;
+    }
+}
+
+class Dog extends Animal {
+    constructor(name, age, color){
+        super(name,age)
+        this.color = color;
+    }
+
+    speak() {
+        return `${this.name} is ${this.color}`;
+    }
+}
+
+let cow = new Animal("cow",)
+let dog = new Dog("Sahin", 20, "red");
+// console.log(dog.speak());
+// console.log(cow.speak());
+
+// 8.
+
